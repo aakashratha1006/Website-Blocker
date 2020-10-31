210f3f81c17dd36bb60151edcb675e3ca7783ffe
Aakash Ratha <aakash.ratha2018@vitstudent.ac.in>
2020-10-31 11:57:52 +0530

    Add files via upload

---
+++ web-blocker.py
@@ -0,0 +1,28 @@
+from datetime import datetime as dt
+
+host_path = 'C:/Windows/System32/drivers/etc/hosts'
+redirect = '127.0.0.1'
+
+website_list = ["www.facebook.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,16): 
+        with open(host_path, 'r+') as file:
+            content=file.read()
+            for site in website_list:
+                if site in content:
+                    pass
+                else:
+                    file.write(redirect + " " + site + "\n")
+        print("All sites are blocked")
+        break
+    else:
+        with open(host_path, "r+") as file:
+            content = file.readlines();
+            file.seek(0)
+            for line in content:
+                if not any(site in line for site in website_list):
+                    file.write(line)
+            file.truncate()
+        print("All sites unblocked")
+        break
